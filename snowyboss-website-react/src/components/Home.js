dea8d2ae501c93c2e1d9a76ad3f7eaebf55896c5
Sn0wyB0ss <zenter2018@gmail.com>
2024-12-27 14:13:59 -0300

    Fixed Age In Home Component

--- snowyboss-website-react/src/components/Home.js
+++ snowyboss-website-react/src/components/Home.js
@@ -1,37 +1,29 @@
-<<<<<<< HEAD
-
-import React from 'react';
-import './Home.css';
-import Bio from './Bio.js'
-=======
 import React from 'react';
 import './Home.css';
 import BioHeader from './BioHeader.js';
->>>>>>> main
 
 class Home extends React.Component {
+
+    returnAge() {
+        var birthDate = new Date("2004-06-15");
+        var currentDate = new Date();
+        var diffDate = currentDate - birthDate;
+        var ageDate = new Date(diffDate);
+        return Math.abs(ageDate.getUTCFullYear - 1970);
+    }
 
     render () {
         return(
             <div className="home">
                 <div className="logo">
-<<<<<<< HEAD
-                    <img src={require("../assets/image/logotest.png")} alt="github"/>
-                </div>
-                <div className="brief">
-                Daniel Soares Alves - 19 Years Old - Brazillian
-                </div>         
-                <Bio></Bio>
-=======
                 <img src={require("../assets/image/logotest.png")} alt="github"/>
                 </div>
                 <div className="brief">
-                Daniel Soares Alves - 19 Years Old - Brazillian
+                Daniel Soares Alves (Sn0wyB0ss) - {this.returnAge()} Years Old - Brazillian
                 </div>     
 
                 <BioHeader></BioHeader>    
                 
->>>>>>> main
             </div>
         );
     }
